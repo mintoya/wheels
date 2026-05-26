66cefd360a1a652b96b08d9d59788b08f4b94172
mintoya <hfm598@gmail.com>
2026-05-25 20:25:55 -0500

    xmake fix

--- xmake.lua
+++ xmake.lua
@@ -1,9 +1,3 @@
-set_project("wheels")
-set_languages("c23")
-
-add_rules("mode.debug", "mode.release")
-
 target("wheels")
-  set_kind("headeronly")
-  add_includedirs(".")
-  add_headerfiles("*.h")
+    set_kind("headeronly")
+    add_headerfiles("*.h", {prefixdir = "wheels"})
