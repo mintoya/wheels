e71be2da7affdf3ceff394a4abf0d2d703bc76c4
Seisie <seisiep@msoe.edu>
2026-06-19 16:12:45 -0400

    cmdline parser simplification

--- cmdline_parser.h
+++ cmdline_parser.h
@@ -1,206 +1,84 @@
-// include in the main file, no guard
-#include "allocator.h"
+#include "fptr.h"
 #include "macros.h"
-#include "mytypes.h"
 #include "print.h"
-#include "shmap.h"
-#include "tu_macros.h"
 
-typedef struct {
-  char *name;
-  enum : u8 {
-    CMDARG_BOOLEAN, // present or not
-    CMDARG_INTEGER,
-    CMDARG_STRING,
-  } kind;
-  char *description;
-} cmdline_option;
+#define TUPLE_GET_4_(a, b, c, d, ...) d
+#define TUPLE_GET_4(...) TUPLE_GET_4_ __VA_ARGS__
 
-tu_def(
-    (cmdline_type, u8),
-    (cmdline_int, i64),
-    (cmdline_bool, bool),
-    (cmdline_long, c8 *)
-);
-struct cmdline_roll_t {
-  i64 value;
-  char *failed;
-};
-struct cmdline_roll_t cmdline_roll(sentList_t(char) n) {
-  char *start = n;
-  usize len = strlen(n);
-  i64 neg = 1;
-  i64 res = 0;
-  enum { hex = 16,
-         bin = 2,
-         dec = 10 } base = dec;
-  if (len > 2) {
-    if (fptr_eq(fp("0x"), ((fptr){2, start}))) {
-      base = hex;
-      start += 2;
-      len -= 2;
-    } else if (fptr_eq(fp("0b"), ((fptr){2, start}))) {
-      base = bin;
-      start += 2;
-      len -= 2;
-    }
-  }
-  if (len > 0 && start[0] == '-') {
-    neg = -1;
-    start++;
-    len--;
+#define TUPLE_GET_5_(a, b, c, d, e, ...) e
+#define TUPLE_GET_5(...) TUPLE_GET_5_ __VA_ARGS__
+
+#define cmd_arg_name_(a, b, ...) b
+#define cmd_arg_name(tuple) cmd_arg_name_ tuple
+
+#define cmd_arg_decl_(type, name, acess, message, ...) type name = VA_SWITCH({}, __VA_ARGS__);
+#define cmd_arg_decl(tuple) cmd_arg_decl_ tuple
+
+#define cmd_arg_type_(type, name, ...) type
+#define cmd_arg_type(tuple) cmd_arg_type_ tuple
+
+#define cmd_arg_param_(name, type, ...) name type
+#define cmd_arg_param(tuple) cmd_arg_param_ tuple
+
+#define cmd_arg_flags_(type, name, flags, ...) flags
+#define cmd_arg_flags(tuple) cmd_arg_flags_ tuple
+
+#define cmd_arg_parse(tuple)                                                                     \
+  {                                                                                              \
+    char *_f[] = (char *[]){REM_PAREN cmd_arg_flags(tuple), nullptr};                            \
+    bool _matched = false;                                                                       \
+    for (int _j = 0; _f[_j]; ++_j) {                                                             \
+      if (fptr_eq(arg, _f[_j])) {                                                                \
+        _matched = true;                                                                         \
+        break;                                                                                   \
+      }                                                                                          \
+    }                                                                                            \
+    if (_matched) {                                                                              \
+      cmd_arg_name(tuple) = _Generic(((cmd_arg_type(tuple)){}), char *: argsp[++i], bool: true); \
+      continue;                                                                                  \
+    }                                                                                            \
   }
 
-  foreach (var_ c, span(start, len)) {
-    switch (c[0]) {
-      case '0' ... '9': {
-        i64 add = c[0] - '0';
-        if (add > base) return ((struct cmdline_roll_t){.failed = "base conversion failure"});
-        res *= base;
-        res += add;
-      } break;
-      case 'a' ... 'f': {
-        i64 add = c[0] - 'a' + 10;
-        if (add > base) return ((struct cmdline_roll_t){.failed = "base conversion failure"});
-        res *= base;
-        res += add;
-      } break;
-      default:
-        return ((struct cmdline_roll_t){.failed = "character not part of any base"});
-    }
-  }
-  return ((struct cmdline_roll_t){res * neg});
-}
-msHmap(cmdline_type) cmdLine_parse(AllocatorV allocator, cmdline_option *options, int nargs, char **args) {
-  var_ opts = sentList_vla(options);
-  var_ result = msHmap_init(allocator, cmdline_type, countof(*opts) + 1);
-  msList(c8 *) positionals = msList_init(allocator, c8 *);
+#define cmd_arg_message_(type, name, flags, message, ...) message
+#define cmd_arg_message(tuple) cmd_arg_message_ tuple
 
-  for (int i = 0; i < nargs; i++) {
-    int found = 0;
-    foreach (var_ opt, vla(*opts)) {
-      if (fptr_eq(fp(args[i]), fp(opt.name))) {
-        found = true;
+#define cmd_arg_name_str_(type, name, ...) #name
+#define cmd_arg_name_str(tuple) cmd_arg_name_str_ tuple
 
-        cmdline_type value;
-        switch (opt.kind) {
-          case CMDARG_BOOLEAN:
-            value = (cmdline_type)tu_of(cmdline_bool, true);
-            break;
-          case CMDARG_INTEGER:
-            if (i + 1 < nargs) {
-              i++;
-              var_ toi = cmdline_roll(args[i]);
-              if (toi.failed) {
-                println("couldnt parse {} using {} , error : {}", opt.name, args[i], toi.failed);
-                exit(1);
-              } else value = (cmdline_type)tu_of(cmdline_int, toi.value);
-            } else {
-              println("{} requires an argument", opt.name);
-              exit(1);
-            }
-            break;
-          case CMDARG_STRING:
-            if (i + 1 < nargs) {
-              i++;
-              value = (cmdline_type)tu_of(cmdline_long, (c8 *)args[i]);
-            } else {
-              println("{} requires an argument", opt.name);
-              exit(1);
-            }
-
-            break;
-          default:
-            unreachable();
-            break;
-        }
-
-        msHmap_set(result, opt.name, value);
-        break;
-      }
-    }
-
-    if (!found)
-      msList_push(allocator, positionals, (c8 *)args[i]);
+#define cmd_arg_usage_print(tuple)                                    \
+  {                                                                   \
+    char *_f[] = (char *[]){REM_PAREN cmd_arg_flags(tuple), nullptr}; \
+    fputs("\t", stdout);                                              \
+    for (usize _j = 0; _f[_j]; ++_j) {                                \
+      if (_j) fputs(",", stdout);                                     \
+      fputs(_f[_j], stdout);                                          \
+    }                                                                 \
+    fputs(":\t", stdout);                                             \
+    fputs(cmd_arg_message(tuple), stdout);                            \
+    fputs("\n", stdout);                                              \
   }
 
-  msHmap_set(result, ((fptr){8, "\0\0\0\0\0\0\0\0"}), (cmdline_type)tu_of(cmdline_long, (c8 *)positionals));
-  return result;
-}
-void cmdLine_deInit(AllocatorV allocator, msHmap(cmdline_type) a) {
-  cmdline_type *buffer = msHmap_get(a, ((fptr){8, "\0\0\0\0\0\0\0\0"}));
-  assertMessage(buffer);
-  tu_match(
-      buffer[0],
-      case (cmdline_long, sl, { msList_deInit(allocator, (c8 **)sl); }),
-      default({ unreachable(); })
-  );
-  msHmap_deinit(a);
-}
-
-#define CMDLINE_GENERATE_MAIN(...)                                    \
-  extern int cmdline_main(cmdline_args args);                         \
-  int main(int argc, char **argv) {                                   \
-    var_ parsed = cmdLine_parse(stdAlloc, (__VA_ARGS__), argc, argv); \
-    int result = cmdline_main(parsed);                                \
-    cmdLine_deInit(stdAlloc, parsed);                                 \
-    return result;                                                    \
+#define cmd_define_usage(...)                     \
+  void cmd_usage(char *prog_name) {               \
+    println();                                    \
+    println("Usage: {} [options]...", prog_name); \
+    println("Options:");                          \
+    APPLY_N(cmd_arg_usage_print, __VA_ARGS__)     \
+    exit(1);                                      \
   }
-
-#define cmdline_get(Map, Name, Type) ({                                      \
-  Type *_res = {0};                                                          \
-  var_ _v = msHmap_get((Map), (Name));                                       \
-  var_ _tt = _Generic(                                                       \
-      (Type *){},                                                            \
-      bool *: CMDARG_BOOLEAN,                                                \
-      int *: CMDARG_INTEGER,                                                 \
-      u32 *: CMDARG_INTEGER,                                                 \
-      u64 *: CMDARG_INTEGER,                                                 \
-      char **: CMDARG_STRING                                                 \
-  );                                                                         \
-  if (_v) {                                                                  \
-    switch (_tt) {                                                           \
-      case CMDARG_BOOLEAN: {                                                 \
-        assertMessage(_v->tag == TU_TAG(cmdline_bool));                      \
-      } break;                                                               \
-      case CMDARG_INTEGER: {                                                 \
-        assertMessage(_v->tag == TU_TAG(cmdline_int));                       \
-      } break;                                                               \
-      case CMDARG_STRING: {                                                  \
-        assertMessage(_v->tag == TU_TAG(cmdline_long));                      \
-      } break;                                                               \
-    }                                                                        \
-    _res = (typeof(_res))((u8 *)_v + offsetof(typeof(_v[0]), cmdline_long)); \
-  }                                                                          \
-  _res;                                                                      \
-})
-void cmdLine_printUsage(char *prog_name, cmdline_option *options) {
-  println();
-  println("Usage: {} [options]...", prog_name);
-  println("Options:");
-
-  var_ opts = sentList_vla(options);
-
-  foreach (var_ opt, vla(*opts)) {
-    char *type_str = "";
-    switch (opt.kind) {
-      case CMDARG_BOOLEAN:
-        type_str = "<bool>";
-        break;
-      case CMDARG_INTEGER:
-        type_str = "<int>";
-        break;
-      case CMDARG_STRING:
-        type_str = "<str>";
-        break;
-    }
-
-    println("  {}:{}:\t{}", opt.name, type_str, opt.description ? opt.description : "");
-  }
-}
-typedef msHmap(cmdline_type) cmdline_args;
-
-char **cmdLine_positionals(cmdline_args args) {
-  var_ v = cmdline_get(args, ((fptr){8, "\0\0\0\0\0\0\0\0"}), char *);
-  return *(char ***)v;
-}
+#define cmd_main(nargs, args, ...)                                                        \
+  int _cmd_main_inner(int _nargss, char **_argss, APPLY_N_C(cmd_arg_param, __VA_ARGS__)); \
+  cmd_define_usage(__VA_ARGS__);                                                          \
+  int main(int nargsp, char *argsp[]) {                                                   \
+    char *args_new[nargsp] = {};                                                          \
+    int nargs_new = 0;                                                                    \
+    APPLY_N(cmd_arg_decl, __VA_ARGS__);                                                   \
+    for (int i = 1; i < nargsp; ++i) {                                                    \
+      char *arg = argsp[i];                                                               \
+      APPLY_N(cmd_arg_parse, __VA_ARGS__)                                                 \
+      args_new[nargs_new++] = arg;                                                        \
+    }                                                                                     \
+    char *ars[] = (char *[]){APPLY_N_C(TUPLE_GET_4, __VA_ARGS__)};                        \
+    return _cmd_main_inner(nargs_new, args_new, APPLY_N_C(cmd_arg_name, __VA_ARGS__));    \
+  }                                                                                       \
+  int _cmd_main_inner(nargs, args, APPLY_N_C(cmd_arg_param, __VA_ARGS__))
