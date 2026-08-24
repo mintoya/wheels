2fdf0773c5279acb1bbc94ede224e65d15d1495c
mintoya <hfm598@gmail.com>
2026-08-24 00:29:51 +0000

    stuff

--- oxmap.h
+++ oxmap.h
@@ -31,19 +31,19 @@ void oxmap_freem(oxmap map);
   #define moxmap_init(allocator, K, V, ...) (moxmap(K, V)) oxmap_new(allocator, sizeof(K), sizeof(V), VA_SWITCH(nullptr, __VA_ARGS__))
   #define moxmap_deinit(map) oxmap_free(moxmap_tox(map))
   #define moxmap_set(map, key, val) ((moxmap_vt(map) *)({ \
-    var_ _k = key;                                        \
-    var_ _v = val;                                        \
+    let _k = key;                                         \
+    let _v = val;                                         \
     (void)sizeof(({ typeof(&_v) _r = (typeof((*map)((oxmap *)0, &_k)) *)0;0; }));                                  \
     oxmap_set(moxmap_tox(map), &_k, &_v);                 \
   }))
   #define moxmap_get(map, key) ((moxmap_vt(map) *)({    \
-    var_ _k = key;                                      \
+    let _k = key;                                       \
     (void)sizeof((typeof((*map)((oxmap *)0, &_k)) *)0); \
     oxmap_get(moxmap_tox(map), &_k);                    \
   }))
   #define moxmap_rem(map, key)                            \
     ({                                                    \
-      var_ _k = key;                                      \
+      let _k = key;                                       \
       (void)sizeof((typeof((*map)((oxmap *)0, &_k)) *)0); \
       oxmap_set(moxmap_tox(map), &_k, nullptr);           \
     })
@@ -58,7 +58,7 @@ void oxmap_freem(oxmap map);
         } _val[0];                      \
       },                                \
       ({                                \
-        var_ _map_eval = map_ptr;       \
+        let _map_eval = map_ptr;        \
         (typeof(_foreach_._foreach_)){  \
             ._m = _map_eval,            \
             ._idx = 0,                  \
@@ -91,7 +91,7 @@ void oxmap_freem(oxmap map);
         } _val[0];                             \
       },                                       \
       ({                                       \
-        var_ _map_eval = map_ptr;              \
+        let _map_eval = map_ptr;               \
         _Static_assert(                        \
             types_eq(                          \
                 typeof(_map_eval),             \
@@ -118,81 +118,81 @@ void oxmap_freem(oxmap map);
         FOREACH_moxmap_cast)
 // }
 i8 test_icmp(const void *a, const void *b) {
-  var_ ai = *(int *)a;
-  var_ bi = *(int *)b;
+  let ai = *(int *)a;
+  let bi = *(int *)b;
   return ai > bi ? -1 : bi > ai ? 1
                                 : 0;
 }
 test_fn(oxmap_basic) {
-  var_ map = oxmap_new(allocator, sizeof(int), sizeof(int), test_icmp);
+  let map = oxmap_new(allocator, sizeof(int), sizeof(int), test_icmp);
   defer { oxmap_free(map); };
 
-  foreach (var_ i, range(0, 100))
+  foreach (let i, range(0, 100))
     oxmap_set(map, REF(i), REF(i * i));
-  foreach (var_ i, range(0, 100)) {
-    var_ g = oxmap_get(map, REF(i));
+  foreach (let i, range(0, 100)) {
+    let g = oxmap_get(map, REF(i));
     test_assert(g);
     test_assert(*(int *)g == i * i);
   }
 
-  var_ ints = acreate(allocator, i8[100]);
+  let ints = acreate(allocator, i8[100]);
   defer { adestroy(allocator, ints); };
 
-  foreach (var_ it, oxmap_iter(map)) {
-    var_ k = *(int *)it.key;
-    var_ v = *(int *)it.val;
+  foreach (let it, oxmap_iter(map)) {
+    let k = *(int *)it.key;
+    let v = *(int *)it.val;
     test_assert(v == k * k);
     (*ints)[k] = 1;
   }
 
-  foreach (var_ i, vlap(ints))
+  foreach (let i, vlap(ints))
     test_assert(i == 1);
 }
 test_fn(oxmap_basic_nosort) {
-  var_ map = oxmap_new(allocator, sizeof(int), sizeof(int), nullptr);
+  let map = oxmap_new(allocator, sizeof(int), sizeof(int), nullptr);
   defer { oxmap_free(map); };
 
-  foreach (var_ i, range(0, 100))
+  foreach (let i, range(0, 100))
     oxmap_set(map, REF(i), REF(i * i));
-  foreach (var_ i, range(0, 100)) {
-    var_ g = oxmap_get(map, REF(i));
+  foreach (let i, range(0, 100)) {
+    let g = oxmap_get(map, REF(i));
     test_assert(g);
     test_assert(*(int *)g == i * i);
   }
 
-  var_ ints = acreate(allocator, i8[100]);
+  let ints = acreate(allocator, i8[100]);
   defer { adestroy(allocator, ints); };
 
-  foreach (var_ it, oxmap_iter(map)) {
-    var_ k = *(int *)it.key;
-    var_ v = *(int *)it.val;
+  foreach (let it, oxmap_iter(map)) {
+    let k = *(int *)it.key;
+    let v = *(int *)it.val;
     test_assert(v == k * k);
     (*ints)[k] = 1;
   }
 
-  foreach (var_ i, vlap(ints))
+  foreach (let i, vlap(ints))
     test_assert(i == 1);
 }
 test_fn(oxmap_macros) {
-  var_ map = moxmap_init(allocator, int, int, test_icmp);
+  let map = moxmap_init(allocator, int, int, test_icmp);
   defer { moxmap_deinit(map); };
-  foreach (var_ i, range(0, 100))
+  foreach (let i, range(0, 100))
     moxmap_set(map, i, i * i);
-  foreach (var_ i, range(0, 100)) {
-    var_ g = moxmap_get(map, i);
+  foreach (let i, range(0, 100)) {
+    let g = moxmap_get(map, i);
     test_assert(g);
     test_assert(*g == i * i);
   }
 
-  var_ ints = acreate(allocator, i8[100]);
+  let ints = acreate(allocator, i8[100]);
   defer { adestroy(allocator, ints); };
 
-  foreach (var_ it, moxmap_iter(map, int, int)) {
+  foreach (let it, moxmap_iter(map, int, int)) {
     test_assert(*it.val == it.key * it.key);
     (*ints)[it.key] = 1;
   }
 
-  foreach (var_ i, vlap(ints))
+  foreach (let i, vlap(ints))
     test_assert(i == 1);
 }
 #endif
@@ -219,12 +219,12 @@ oxmap *oxmap_new(allocfn allocator, u32 ksize, u32 vsize, itypeof(oxmap, cmp) cm
   return r;
 }
 void oxmap_freem(oxmap map) {
-  var_ allocator = map.allocator;
+  let allocator = map.allocator;
   sList_free(allocator, map.keys, map.ksize);
   sList_free(allocator, map.vals, map.vsize);
 }
 void oxmap_free(oxmap *map) {
-  var_ allocator = map->allocator;
+  let allocator = map->allocator;
   oxmap_freem(*map);
   adestroy(allocator, map);
 }
@@ -236,7 +236,7 @@ void *oxmap_val_key(const oxmap *map, const void *val) {
 }
 void *oxmap_set(oxmap *map, const void *key, const void *val) {
   if (!key) return nullptr;
-  var_ pos = bbsearch(key, map->keys->buf, map->keys->length, map->ksize, map->cmp);
+  let pos = bbsearch(key, map->keys->buf, map->keys->length, map->ksize, map->cmp);
   usize idx = ((u8 *)pos.p - map->keys->buf) / map->ksize;
 
   if (val) {
@@ -256,7 +256,7 @@ void *oxmap_set(oxmap *map, const void *key, const void *val) {
 }
 void *oxmap_get(const oxmap *map, const void *key) {
   if (!key) return nullptr;
-  var_ pos = bbsearch(key, map->keys->buf, map->keys->length, map->ksize, map->cmp);
+  let pos = bbsearch(key, map->keys->buf, map->keys->length, map->ksize, map->cmp);
   if (pos.f) return oxmap_key_val(map, pos.p);
   return nullptr;
 }
